a72a45f212d0a70bee2bc27c8c1aa3ef722c89d8
unknown <masymas99>
2024-09-28 00:48:43 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/semsar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.semsar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(),
+    );
+  }
+}
